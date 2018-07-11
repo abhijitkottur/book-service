7877edd8a354c865e95a10d9c04e03566ff196de
Abhijit Kottur <abhijit.kottur@winning.com.au>
2018-07-12 09:36:32 +1000

    changes to port

--- src/main/java/com/abhijit/bookservice/BookServiceImpl.java
+++ src/main/java/com/abhijit/bookservice/BookServiceImpl.java
@@ -1,11 +1,15 @@
 package com.abhijit.bookservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl implements BookService {
 
+	@Autowired
+	private Environment environment;
+	
 	@Autowired
 	private BookRepository bookRepository;
 	
@@ -17,6 +21,7 @@ public class BookServiceImpl implements BookService {
 		dto.setId(entity.getId());
 		dto.setName(entity.getName());
 		dto.setPrice(entity.getPrice());
+		dto.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		
 		return dto;
 	}
