dc9b5d114f2668f6cac01692d6f72ac5adb80574
Abhijit Kottur <abhijit.kottur@winning.com.au>
2018-07-22 10:23:54 +1000

    zipkin changes

--- src/main/java/com/abhijit/bookservice/BookController.java
+++ src/main/java/com/abhijit/bookservice/BookController.java
@@ -1,5 +1,7 @@
 package com.abhijit.bookservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookController {
 
+	private static final Logger log = LoggerFactory.getLogger(BookController.class);
+	
 	@Autowired
 	private BookService bookService;
 	
 	@GetMapping(value = "/books/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public BookDto findById(@PathVariable String id) {
+		log.info("Book {} requested", id);
 		return bookService.findById(Long.parseLong(id));
 	}
 	
--- src/main/java/com/abhijit/bookservice/BookServiceApplication.java
+++ src/main/java/com/abhijit/bookservice/BookServiceApplication.java
@@ -3,6 +3,9 @@ package com.abhijit.bookservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -10,5 +13,10 @@ public class BookServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
